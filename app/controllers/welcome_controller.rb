5d02a2608357a157beb922434cd6133ac86b2718
Kazuaki Takemoto <libkazz@gmail.com>
2017-11-19 11:53:12 +0900

    :bug: sorry..

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @hot_movies= Movie.top10(:movie).first(5)
-    @hot_musics= Music.top10(:music).first(5)
+    @hot_movies= Movie.top10(Genre.find('33')).first(5)
+    @hot_musics= Music.top10(Genre.find('34')).first(5)
   end
 end
