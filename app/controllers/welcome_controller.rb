6bede7772d49d969737a772fd2d47f108c71a4c7
Yuzo Kase <7413025@alumni.tus.ac.jp>
2018-01-20 08:35:56 +0000

    テストが失敗したのでイコールの前にスペースを追加

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  DISPLAY_SIZE= 4
+  DISPLAY_SIZE = 4
   def index
     @hot_movies = Medium.where(category: '映画').limit(DISPLAY_SIZE)
     @hot_musics = Medium.where(category: 'ミュージック').limit(DISPLAY_SIZE)
