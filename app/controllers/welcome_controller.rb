d511ea489506f3feda458341496130f2d78e0a5f
Yuzo Kase <7413025@alumni.tus.ac.jp>
2018-01-20 08:12:13 +0000

    トップページのコンテンツ数を４つに変更した

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @hot_movies = Medium.where(category: '映画').limit(5)
-    @hot_musics = Medium.where(category: 'ミュージック').limit(5)
+    @hot_movies = Medium.where(category: '映画').limit(4)
+    @hot_musics = Medium.where(category: 'ミュージック').limit(4)
   end
 end
