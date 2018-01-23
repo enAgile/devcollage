8b083e2c7d0b02e0bdfe83a49734b64d434b1e4d
jun hattori <jundeiiya@gmail.com>
2018-01-23 11:30:11 +0000

    修正しました。

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -13,5 +13,5 @@
 class Inquiry < ApplicationRecord
   validates :inquirer, presence: true
   validates :email, presence: true
-  validates :body, presence: true , length: { maximum: 300 }
+  validates :body, presence: true, length: { maximum: 300 }
 end
