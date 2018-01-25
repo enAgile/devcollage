6ff84686263e814a4188f8595b8cf6135ddd41b0
Harumi Minamoto <minahr.0.a@gmail.com>
2018-01-25 11:11:02 +0000

    SideCIの指摘を再修正

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -10,14 +10,6 @@
 #  updated_at :datetime         not null
 #
 
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "はメールアドレスではありません")
-    end
-  end
-end
-
 class Inquiry < ApplicationRecord
   validates :inquirer, presence: true
   validates :email, presence: true, email: true
