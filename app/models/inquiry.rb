94e1d460d6afe3a378681d10fe3fd7e73ba2217b
Harumi Minamoto <minahr.0.a@gmail.com>
2018-01-25 10:54:42 +0000

    メールアドレスが正しい場合のみ問い合わせを受け付けるように変更

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -10,8 +10,16 @@
 #  updated_at :datetime         not null
 #
 
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "はメールアドレスではありません")
+    end
+  end
+end
+
 class Inquiry < ApplicationRecord
   validates :inquirer, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, email: true
   validates :body, presence: true, length: { maximum: 300 }
 end
