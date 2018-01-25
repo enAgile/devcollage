8d836cbd17a3620862da9a16b745c106ca585db8
Harumi Minamoto <minahr.0.a@gmail.com>
2018-01-25 11:04:27 +0000

    SideCIの指摘を修正

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,7 +1,6 @@
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "はメールアドレスではありません")
-    end
+    return if value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    record.errors[attribute] << (options[:message] || 'はメールアドレスではありません')
   end
 end
