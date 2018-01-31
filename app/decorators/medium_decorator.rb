350abefbda5d33e5f44c58d14330ae127c17012a
Yuya Tabata <yuya1212h4.066@gmail.com>
2018-01-31 21:21:37 +0900

    price_with_taxにメソッド名を変更

--- app/decorators/medium_decorator.rb
+++ app/decorators/medium_decorator.rb
@@ -1,5 +1,5 @@
 module MediumDecorator
-  def price_add_tax
+  def price_with_tax
     number_to_currency(price_amount * 1.08, unit: price_currency == 'JPY' ? '¥' : '$', precision: 0) + '(税込)'
   end
 end
