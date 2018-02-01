edc31829a92b9cc2017f48e01df8e3fae4e8cce2
Harumi Minamoto <minahr.0.a@gmail.com>
2018-02-01 12:26:08 +0000

    お問い合わせフォームを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :genres, only: %i(show)
   resources :media, only: %i(show)
+  resources :inquiries, only: %i(new create)
   root to: 'welcome#index'
 
   if Rails.env.development?
